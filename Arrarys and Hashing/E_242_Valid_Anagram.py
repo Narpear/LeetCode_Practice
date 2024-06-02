e38d6f2f1222b4c21d0f9dda0564164f82c25798
Narpear <prer.kulk@gmail.com>
2024-06-02 11:02:58 +0530

    Alternate solution 242

--- Arrarys and Hashing/E_242_Valid_Anagram.py
+++ Arrarys and Hashing/E_242_Valid_Anagram.py
@@ -1,4 +1,25 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         return (sorted(s) == sorted(t))
-        
+
+
+# #alternate solution
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         s_count = {}
+#         t_count = {}
+
+#         for character in s:
+#             if character in s_count:
+#                 s_count[character] += 1
+#                 continue
+#             s_count[character] = 1
+
+#         for character in t:
+#             if character in t_count:
+#                 t_count[character] += 1
+#                 continue
+#             t_count[character] = 1
+
+#         if s_count==t_count:
+#             return True
+#         return False
