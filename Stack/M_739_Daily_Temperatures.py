ee43d437dbb4e51dbbf59b450f0aa5d528bf1bb8
Narpear <prer.kulk@gmail.com>
2024-06-12 16:32:07 +0530

    daily temperatures

---
+++ Stack/M_739_Daily_Temperatures.py
@@ -0,0 +1,14 @@
+# monotonic decreasing stack
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        
+        answer = [0]*len(temperatures)
+        stack = []  # pairs of (temp, index)
+
+        for i, temp in enumerate(temperatures):
+            while stack and temp>stack[-1][0]:
+                stackTemp, stackIndex = stack.pop()
+                answer[stackIndex] = (i-stackIndex)
+            stack.append([temp, i])           
+
+        return answer
