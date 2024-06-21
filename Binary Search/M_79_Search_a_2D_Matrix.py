4b7ca4c04bb40925a51b61a771961a569c66e72e
Narpear <prer.kulk@gmail.com>
2024-06-21 21:14:35 +0530

    search a 2D matrix

---
+++ Binary Search/M_79_Search_a_2D_Matrix.py
@@ -0,0 +1,29 @@
+from typing import List
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        row_number=-1
+        m = len(matrix)
+        n = len(matrix[0])
+
+        for i in range(m-1):
+            if target>=matrix[i][0] and target<matrix[i+1][0]:
+                row_number = i
+                break
+        if row_number==-1:
+            row_number = m-1
+
+        left = 0
+        right = n-1
+        while(left<=right):
+            mid = (left+right)//2
+            if matrix[row_number][mid]>target:
+                right = mid-1
+            elif matrix[row_number][mid]<target:
+                left = mid+1
+            else:
+                if matrix[row_number][mid]==target:
+                    return True
+        
+        return False
+
+
