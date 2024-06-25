682ae2491bb67b08b0ed588b7f91c465d28f0472
Narpear <prer.kulk@gmail.com>
2024-06-25 17:10:46 +0530

    number of 1 bits

--- Bit Manipulation/E_191_Number_of_1_Bits.py
+++ Bit Manipulation/E_191_Number_of_1_Bits.py
@@ -2,6 +2,6 @@ class Solution:
     def hammingWeight(self, n: int) -> int:
         count = 0
         while n>0:
-            count = count + (n%2)
+            count += n%2
             n = n//2
         return count
