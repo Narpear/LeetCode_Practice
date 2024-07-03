0188b75676d790ca1640bc6f61a5d7e89e5b56e2
Narpear <prer.kulk@gmail.com>
2024-07-03 10:37:46 +0530

    moores voting algorithm

--- Arrarys and Hashing/E_169_Majority_Element.py
+++ Arrarys and Hashing/E_169_Majority_Element.py
@@ -1,15 +1,29 @@
 from typing import List
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        counts = {}
-        n = len(nums)
-        threshold = n//2
+        # hashmap solution
+        # counts = {}
+        # n = len(nums)
+        # threshold = n//2
 
+        # for number in nums:
+        #     if number in counts:
+        #         counts[number] += 1
+        #     else:
+        #         counts[number] = 1
+        #     if counts[number] > threshold:
+        #         return number
+        # return
+
+        # moore's majority voting algorithm
+        count = 0
+        element = 0
         for number in nums:
-            if number in counts:
-                counts[number] += 1
+            if count==0:
+                element = number
+            if number==element:
+                count += 1
             else:
-                counts[number] = 1
-            if counts[number] > threshold:
-                return number
-        return
+                count -= 1
+            
+        return element
