3f5c6a974f3b30655c5dfadc6802b008228c84db
Narpear <prer.kulk@gmail.com>
2024-07-05 10:00:54 +0530

    subarray sum equals k

---
+++ Arrarys and Hashing/M_560_Subarray_Sum_Equals_K.py
@@ -0,0 +1,21 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        result = 0
+        current_sum = 0
+        prefix_sums = {0:1}
+
+        for number in nums:
+            current_sum += number
+            difference = (current_sum - k)
+            
+            # if difference in prefix_sums:
+            #     result += prefix_sums[difference]
+            # if current_sum in prefix_sums:
+            #     prefix_sums[current_sum] += 1
+            # else:
+            #     prefix_sums[current_sum] = 1
+
+            result += prefix_sums.get(difference, 0)
+            prefix_sums[current_sum] = 1 + prefix_sums.get(current_sum, 0)
+        
+        return result
