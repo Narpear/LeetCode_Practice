88edb89beb252735b6b78914430a24cd11f0657e
Narpear <prer.kulk@gmail.com>
2024-08-11 17:03:09 +0530

    fixed typo in reverse linked list

--- Linked Lists/E_206_Reverse_Linked_List.py
+++ Linked Lists/E_206_Reverse_Linked_List.py
@@ -9,7 +9,7 @@ class Solution:
         curr = head
         while curr:
             next_node = curr.next
-            curr.next = prevs
+            curr.next = prev
             prev = curr
             curr = next_node
         return prev
